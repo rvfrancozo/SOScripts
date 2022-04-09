9b64bf1ac1d4c7917be3bbd9d70f16f3ff925fe4
Rafael Françozo <rvfrancozo@gmail.com>
2022-04-09 10:28:11 -0400

    Cifra de Hill2 Completa

--- hillhash.c
+++ hillhash.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
     int chave[2][2];
+    int chavedecod[2][2];
     int msgcode[255];
     int msgcifrada[255];
     int determinante;
@@ -17,7 +18,7 @@ void getChave() {
     }
 }
 
-void cifra(char mensagem[255]) {
+const char* cifra(char mensagem[255]) {
     char mensagemcifrada[strlen(mensagem)];
     //Cifra a Mensagem
     for(int i = 0; i < strlen(mensagem); i++) {
@@ -35,10 +36,18 @@ void cifra(char mensagem[255]) {
         ++i;
     }
 
-    printf("mensagem cifrada: ");
+    //printf("mensagem cifrada: ");
+    char result[strlen(mensagem)];
     for(int i = 0; i < strlen(mensagem); i++) {
-        printf("%c",alfabeto[msgcifrada[i]]);
+        //printf("%c",alfabeto[msgcifrada[i]]);
+        result[i] = alfabeto[msgcifrada[i]];
     }
+    char *saida;
+
+    saida = malloc(sizeof(char) * (strlen(result) + 1));
+    strncpy(saida,result,strlen(mensagem));
+
+    return saida;
 }
 
 void showMatrix() {
@@ -53,11 +62,15 @@ void showInversa() {
 
 void showDecodificadora() {
     int x = strlen(alfabeto);
+    chavedecod[0][0] = chave[1][1]*getImm()%x > 0 ? chave[1][1]*getImm()%x : chave[1][1]*getImm()%x + x;
+    chavedecod[0][1] = -chave[0][1]*getImm()%x > 0 ? -chave[0][1]*getImm()%x : -chave[0][1]*getImm()%x + x;
+    chavedecod[1][0] = -chave[1][0]*getImm()%x > 0 ?-chave[1][0]*getImm()%x : -chave[1][0]*getImm()%x + x;
+    chavedecod[1][1] = chave[0][0]*getImm()%x > 0 ? chave[0][0]*getImm()%x : chave[0][0]*getImm()%x + x;
     printf("A Matriz decodificadora é: \n%d\t%d\n%d\t%d\n", 
-    chave[1][1]*getImm()%x > 0 ? chave[1][1]*getImm()%x : chave[1][1]*getImm()%x + x,
-    -chave[0][1]*getImm()%x > 0 ? -chave[0][1]*getImm()%x : -chave[0][1]*getImm()%x + x,
-    -chave[1][0]*getImm()%x > 0 ?-chave[1][0]*getImm()%x : -chave[1][0]*getImm()%x + x,
-    chave[0][0]*getImm()%x > 0 ? -chave[1][0]*getImm()%x : -chave[1][0]*getImm()%x + x
+    chavedecod[0][0],
+    chavedecod[0][1],
+    chavedecod[1][0],
+    chavedecod[1][1]
     );
 }
 
@@ -71,7 +84,39 @@ int getImm() {
     return imm;
 }
 
-void main() {
+const char* decifra(char mensagem[255]) {
+
+    for(int i = 0; i < strlen(mensagem); i++) {
+        for(int j = 0; j < strlen(alfabeto); j++) {
+            if(mensagem[i] == alfabeto[j]) {
+                msgcode[i] = j;
+                break;
+            }
+        }
+    }
+
+    for(int i = 0; i < strlen(mensagem); i++) {
+        msgcifrada[i] = (chavedecod[0][0]*msgcode[i] + (chavedecod[0][1])*msgcode[i+1])%strlen(alfabeto);
+        msgcifrada[i+1] = (chavedecod[1][0]*msgcode[i] + (chavedecod[1][1])*msgcode[i+1])%strlen(alfabeto);
+        ++i;
+    }
+
+    //printf("\nmensagem Decifrada: ");
+    char result[strlen(mensagem)];
+    for(int i = 0; i < strlen(mensagem); i++) {
+        //printf("%c",alfabeto[msgcifrada[i]]);
+        result[i] = alfabeto[msgcifrada[i]];
+    }
+
+    char *saida;
+
+    saida = malloc(sizeof(char) * (strlen(result) + 1));
+    strncpy(saida,result,strlen(mensagem));
+
+    return saida;
+}
+
+int main() {
     char mensagem[255];
 
     puts("Informe a  mensagem que deseja encriptar: ");
@@ -88,7 +133,10 @@ void main() {
     printf("O determinante é: %d\n", getDeterminante());
     printf("O Inverso Multiplicativo Modular é: %d\n", getImm());
 
-    cifra(mensagem);
+    printf("\nA mensagem cifrada é: %s \n",cifra(mensagem));
+    printf("\nA mensagem Decifrada é: %s \n", decifra(cifra(mensagem)));
 
     printf("\n");
+
+    return 0;
 }
